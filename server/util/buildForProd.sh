7385a995486dfd931eb1a0c71e36ca53722982b9
Lawrence F <lawrence.f@happiestminds.com>
2020-02-19 17:06:12 +0530

    webpack added as dev dependency

--- server/util/buildForProd.sh
+++ server/util/buildForProd.sh
@@ -13,18 +13,3 @@ tsc --sourceMap false
 
 
 ### Bundle FrontEnd ###
-
-# Create the directory for React
-mkdir -p ./build/public/react/
-
-# Navigate to the react directory
-cd ./src/public/react
-
-# Build React code
-npm run build
-
-# Rename the folder
-mv dist demo-react
-
-# Move the contains to the build/ dir
-mv demo-react ../../../build/public/react/
